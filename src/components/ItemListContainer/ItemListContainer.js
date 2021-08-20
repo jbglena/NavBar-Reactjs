aad848bd82e41fbca6cef43dd6fbf55702462295
Julieta Glenadel <83045778+jbglena@users.noreply.github.com>
2021-08-19 22:20:13 -0300

    Catálogo con MAPS y Promises- ejercicio 6

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './ItemListContainer.css'
+import '..Item/Item'
+import Item from '../Item/Item'
+
 
     function ItemListContainer () {
         return (
         <div className='bienvenida'>
           <h1>Te damos la bienvenida a nuestro sitio!</h1>
+          <Item />
          </div>
          ) }
 
