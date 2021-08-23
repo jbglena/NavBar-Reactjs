9df1827f796225187178eebfede5ab922b5d8c01
Julieta Glenadel <83045778+jbglena@users.noreply.github.com>
2021-08-22 21:52:10 -0300

    Desafío: Catálogo con MAPS y Promises

---
+++ src/components/Data/Productos.js
@@ -0,0 +1,7 @@
+export const ListaProductos = [
+    { id: 1, title: 'Vestido Snake', price: 2000, pictureUrl: '/src/components/Img/images (1).jfif' , category: 'vestidos'},
+    { id: 2, title: 'Remera Batik', price: 1700,  pictureUrl: '/src/components/Img/images (2).jfif' , category: 'tops y remeras' },
+    { id: 3, title: 'Botas', price: 4990,  pictureUrl: '/src/components/Img/images (3).jfif' ,category: 'calzado'},
+    { id: 4, title: 'Camisa shine', price: 6399,  pictureUrl: '/src/components/Img/images (4).jfif' , category: 'camisas' },
+    { id: 6, title: 'Top transparecia', price: 1950,  pictureUrl: '/src/components/Img/images.jfif',  category: 'tops y remeras' }
+];
